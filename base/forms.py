532692ceda8d65597b347107bbeeee1b37da9223
Edgardo Ortiz <89225788+GSGEdgardo@users.noreply.github.com>
2023-02-21 05:29:13 +0000

    Actualización sistema de recomendación y verificaciones Excel

--- base/forms.py
+++ base/forms.py
@@ -3,14 +3,14 @@ from django.core.validators import FileExtensionValidator
 
 class FormLixiviacion(forms.Form):
     Granulometria = forms.FloatField()
-    RatioIrrigacion = forms.FloatField()
-    AcidoTotalAnadido = forms.FloatField()
-    AlturaPila = forms.FloatField()
-    LeyCuTotal = forms.FloatField()
-    LeyCO3 = forms.FloatField()
-    RL = forms.FloatField()
-    DiasOperacion = forms.FloatField()
-    CuSoluble = forms.FloatField()
+    Ratio_Irrigacion = forms.FloatField()
+    Acido_Total_Añadido = forms.FloatField()
+    Altura_Pila = forms.FloatField()
+    Ley_Cu_Total = forms.FloatField()
+    Ley_CO3 = forms.FloatField()
+    Ratio_Lixiviado = forms.FloatField()
+    Dias_Operacion = forms.FloatField()
+    Cu_Soluble = forms.FloatField()
 
 class excelFormLixiviacion(forms.Form):
     archivo = forms.FileField(validators = [FileExtensionValidator(allowed_extensions=["xlsx"])])
--- webPractica/settings.py
+++ webPractica/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -107,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
@@ -121,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
